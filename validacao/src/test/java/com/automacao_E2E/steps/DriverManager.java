89d6873b121308fc96cefbd1d272751f6a6d3c44
Jose Ailton <ailtonzamosouza12@gmail.com>
2025-03-18 16:34:49 -0300

    Corrigindo configuração de Java no workflow

--- validacao/src/test/java/com/automacao_E2E/steps/DriverManager.java
+++ validacao/src/test/java/com/automacao_E2E/steps/DriverManager.java
@@ -6,24 +6,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class DriverManager {
     private static WebDriver driver;
 
     // Método para inicializar o driver
     public static WebDriver getDriver() {
         if (driver == null) {
-           WebDriverManager.chromedriver().setup();
-             ChromeOptions options = new ChromeOptions();    
-             options.addArguments("--remote-allow-origins=*");
-             options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-             WebDriver driver = new ChromeDriver(options);   
+               // WebDriverManager.chromedriver().setup();
+       // ChromeOptions options = new ChromeOptions();
+    
+      //  options.addArguments("--remote-allow-origins=*");
+     //   options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+     //     driver = new ChromeDriver(options);   
 
 
 
 
-            //driver = new ChromeDriver();
+            driver = new ChromeDriver();
             driver.manage().window().maximize();
        //     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
